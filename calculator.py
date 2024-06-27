f50874f4868513abf2d97a03077bca476903005c
Gargi Barve <gargibarve30@gmail.com>
2024-06-27 11:01:30 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,46 @@
+n1=int(input("Enter First number \n"))
+n2=int(input("Enter Second number \n"))
+
+def add(n1,n2):
+  return n1+n2
+
+def sub(n1,n2):
+  return(n1-n2)
+
+def mul(n1,n2):
+    return(n1*n2)
+
+def div(n1,n2):
+  return(n1/n2)
+
+while True:
+  print("Select the operation to be performed:\n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division \n")
+  
+  Operation=input("Enter choice (1/2/3/4) \n")
+
+  if Operation  in('1','2','3','4'):
+    try:
+      if Operation == "1":
+        print(n1,"+",n2,"=",add(n1,n2))
+
+      elif Operation =="2":
+        print(n1,"-",n2,"=",sub(n1,n2))
+
+      elif Operation == "3":
+        print(n1,"*",n2,"=",mul(n1,n2))
+
+      elif Operation == "4":
+        print(n1,"/",n2,"=",div(n1,n2))
+
+    except ZeroDivisionError:
+      print("Cannot divide by zero")
+
+  else:
+    print("Invalid Option")
+
+  next_calc = input("Continue next calculation? (y/n/yes/no/Yes/No):\n")
+
+  if next_calc.lower() in ("n", "no"):
+    break
+  else:
+    print("Invalid input")
